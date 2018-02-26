cb855511ddd9117de6b81c78a03e052b8cf6b234
Kiv Lo <kivlokw@gmail.com>
2018-02-26 15:34:53 -0500

    Add POST /todos route and test out with postman.

--- server/server.js
+++ server/server.js
@@ -1,3 +1,26 @@
-let {mongoose} = require('./db/moongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res) => {
+  var todo = new Todo({
+    text: req.body.text
+  })
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  })
+  .catch((err) => {
+    res.status(400).send(err)
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Listening to port:3000');
+})
