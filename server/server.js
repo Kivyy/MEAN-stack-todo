af5335f5435c85edbed1bb7b600255a580dad09e
Kiv Lo <kivlokw@gmail.com>
2018-02-26 15:54:59 -0500

    add get/todos

--- server/server.js
+++ server/server.js
@@ -21,6 +21,15 @@ app.post('/todos', (req,res) => {
   })
 })
 
+app.get('/todos', (req,res) => {
+  Todo.find().then((todos) => {
+    res.send({todos})
+  })
+  .catch((err) => {
+    res.send(err)
+  })
+})
+
 app.listen(3000, () => {
   console.log('Listening to port:3000');
 })
