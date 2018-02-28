22f08c73df0f28428dfd1851b6ddf47b38ba26f5
Kiv Lo <kivlokw@gmail.com>
2018-02-28 13:08:04 -0500

    implement PATCH /todos/:id api route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 const app = express();
 
 let {mongoose} = require('./db/mongoose');
@@ -33,7 +34,7 @@ app.get('/todos', (req,res) => {
 
 app.get('/todos/:id', (req,res) => {
    var id = req.params.id;
-   if(!objectID.isValid(id)){
+   if(!ObjectID.isValid(id)){
      return res.status(404).send();
    }
 
@@ -49,7 +50,7 @@ app.get('/todos/:id', (req,res) => {
 
 app.delete('/todos/:id',(req,res) => {
   var id = req.params.id;
-  if(!objectID.isvalid(id)){
+  if(!ObjectID.isValid(id)){
     return res.status(404).send()
   }
 
@@ -58,6 +59,33 @@ app.delete('/todos/:id',(req,res) => {
   })
   .catch((err) => {
     res.status(404).send();
+  })
+})
+
+app.patch('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body,['text','completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send()
+  }
+
+  if(_.isBoolean(body.completed) && body.completed ){
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id,{$set: body},{new: true})
+  .then((todo) => {
+    if(!todo){
+      return res.status(404).send()
+    }
+    res.send({todo});
+  })
+  .catch((err) => {
+     res.status(400).send();
   })
 })
 
