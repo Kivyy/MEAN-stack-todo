f45ad95a2a51d7008430097f316ca3bf5d76eefc
Kiv Lo <kivlokw@gmail.com>
2018-03-05 13:13:26 -0500

    ADD /users/login route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 const app = express();
 
 let {mongoose} = require('./db/mongoose');
@@ -106,6 +107,20 @@ app.post('/users', (req,res) => {
 
 app.get('/users/me', authenticate, (req,res)=> {
   res.send(req.user);
+})
+
+app.post('/users/login',(req,res) => {
+  var body = _.pick(req.body,['email','password'])
+  User.findByCredentials(body.email, body.password)
+  .then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+  })
+  .catch((err) => {
+    res.status(400).send();
+  })
+
 })
 
 app.listen(3000, () => {
