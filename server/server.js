589786551944770982d885459d5c7e3866b48fa6
Kiv Lo <kivlokw@gmail.com>
2018-03-05 12:05:15 -0500

    implement authenticate middleware

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const app = express();
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
+let {authenticate} = require('./middleware/authenticate');
 
 app.use(bodyParser.json());
 
@@ -100,6 +101,11 @@ app.post('/users', (req,res) => {
   }).catch((err) => {
     res.status(400).send(err);
   })
+})
+
+
+app.get('/users/me', authenticate, (req,res)=> {
+  res.send(req.user);
 })
 
 app.listen(3000, () => {
