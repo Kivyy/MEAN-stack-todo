db7047fe6ebc15a8a8b84d7295393bf90d9b5660
Kiv Lo <kivlokw@gmail.com>
2018-03-07 14:38:56 -0500

    add secure route to GET /Todos and POST /todos

--- server/models/todo.js
+++ server/models/todo.js
@@ -12,6 +12,10 @@ let Todo = mongoose.model('Todo',{
   completedAt: {
     type: Number,
     default: null,
+  },
+  _creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    require: true,
   }
 });
 
--- server/server.js
+++ server/server.js
@@ -12,10 +12,11 @@ let {authenticate} = require('./middleware/authenticate');
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req,res) => {
+app.post('/todos', authenticate, (req,res) => {
   var todo = new Todo({
-    text: req.body.text
-  })
+    text: req.body.text,
+    _creator: req.user._id
+  });
 
   todo.save().then((doc) => {
     res.send(doc);
@@ -25,8 +26,10 @@ app.post('/todos', (req,res) => {
   })
 })
 
-app.get('/todos', (req,res) => {
-  Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req,res) => {
+  Todo.find({
+    _creator: req.user._id
+  }).then((todos) => {
     res.send({todos})
   })
   .catch((err) => {
